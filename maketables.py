70023e2758872581b934b5f2379b537127135567
Peter Foley <pefoley2@pefoley.com>
2015-05-22 19:14:06 -0400

    make sure that at least one channel is configured, fix #1

--- maketables.py
+++ maketables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import peewee
+import re
 from models import tables
 from config import database
 import chanconfig
@@ -14,3 +15,16 @@ default_channel_config = {"url": False}
 
 for k, v in default_channel_config.items():
     chanconfig.set_config_key("*", k, v)
+
+valid = False
+channels = ""
+chanregex = '[&#+!][^, \x07]+'
+while not valid:
+    channels = input("Please enter one or more comma-delimited channels that you want the bot to automatically connect to: ")
+    valid = True if channels else False
+    for channel in channels.split(','):
+        if not re.match(chanregex, channel):
+            valid = False
+
+for channel in channels.split(','):
+    chanconfig.set_config_key(channel, 'join', True)
