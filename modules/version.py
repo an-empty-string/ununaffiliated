67e9a9ada3581139dafb9b2a81a9a66be191793b
Fox Wilson <fwilson@fwilson.me>
2015-05-23 11:34:49 -0400

    get current version from git

--- modules/version.py
+++ modules/version.py
@@ -1,5 +1,7 @@
 from modules.commands import command
+import subprocess
 
 @command("version")
 def version(bot, data, args):
-    bot.say(data["reply_target"], "fwilson's bot framework version 0.0.1")
+    commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().splitlines()[0]
+    bot.say(data["reply_target"], "ununaffiliated git rev. {}".format(commit))
