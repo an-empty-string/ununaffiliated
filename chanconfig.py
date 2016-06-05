7548a9293a3a21e7a988c7870d2aaeedbae847e4
Fox Wilson <fwilson@shell0.mgmt.fwilson.me>
2016-06-04 21:03:05 -0400

    restore chanconfig

--- chanconfig.py
+++ chanconfig.py
@@ -1,1 +1,19 @@
-utils/chanconfig.py
+from models import ChannelConfig
+import json
+
+def get_config_key(channel, key):
+    r = list(ChannelConfig.select().where(ChannelConfig.channel == channel, ChannelConfig.key == key))
+    if not r:
+        r = list(ChannelConfig.select().where(ChannelConfig.channel == "*", ChannelConfig.key == key))
+    if not r:
+        return None
+
+    return json.loads(r[0].value)
+
+def set_config_key(channel, key, value):
+    value = json.dumps(value)
+    r = list(ChannelConfig.select().where(ChannelConfig.channel == channel, ChannelConfig.key == key))
+    if r:
+        ChannelConfig.update(value=value).where(ChannelConfig.channel == channel, ChannelConfig.key == key).execute()
+    else:
+        ChannelConfig.create(channel=channel, key=key, value=value)
